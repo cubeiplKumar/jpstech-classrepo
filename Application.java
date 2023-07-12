3b89b1a76cfb43fac158c7dbb67a66faad13e259
cubeiplkumar <emailanilkumarm@gmail.com>
2023-07-12 22:22:00 +0530

    new method added into Application file by John

--- Application.java
+++ Application.java
@@ -22,4 +22,24 @@ class Main {
       e.getStackTrace();
     }
   }
+  #feature201 changes made by John for ICICI Bank Project 
+  public static void main(String[] args) {
+
+    char[] array = new char[100];
+    try {
+      // Creates a reader using the FileReader
+      FileReader input = new FileReader("input.txt");
+
+      // Reads characters
+      input.read(array);
+      System.out.println("Data in the file:");
+      System.out.println(array);
+
+      // Closes the reader
+      input.close();
+    }
+    catch(Exception e) {
+      e.getStackTrace();
+    }
+	
 }
