22e326f2673a964b3bda4588b3775a7526b656b7
cubeiplkumar <emailanilkumarm@gmail.com>
2023-07-12 22:27:13 +0530

    feature202 changes

--- Application.java
+++ Application.java
@@ -22,4 +22,19 @@ class Main {
       e.getStackTrace();
     }
   }
+  # feature202 changes made by Ana on CreditCards Module
+  public static void main(String[] args) {
+
+    // creates a file object
+    File file = new File("file.txt");
+
+    // deletes the file
+    boolean value = file.delete();
+    if(value) {
+      System.out.println("The File is deleted.");
+    }
+    else {
+      System.out.println("The File is not deleted.");
+    }
+  }
 }
